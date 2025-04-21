e635870572c2c9ed5b28de54b8f2a6209755c9b0
StasWW <01102006stas@gmail.com>
2025-04-21 22:40:55 +0300

    minorChanges

---
+++ src/Components/side-bar/add-task/addTask.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './addTask.css';
+
+class AddTask extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state ({
+            taskCount: 0,
+        })
+    }
+    createTask () {
+        const renderPlace = document.getElementsByClassName('taskHolder');
+    }
+    render () {
+        <div className="addTask">
+
+        </div>
+    }
+}
+
+export default AddTask;
