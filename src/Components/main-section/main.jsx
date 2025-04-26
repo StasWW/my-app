ad2beaadca5ca62ff6b04b0d577f6f0fdcfedd94
StasWW <01102006stas@gmail.com>
2025-04-26 21:01:38 +0300

    minorfix

--- src/Components/main-section/main.jsx
+++ src/Components/main-section/main.jsx
@@ -1,6 +1,7 @@
 import './main.css';
 import Task from '../individual-task/task';
 import { useTasks } from '../../context';
+import AddTask from '../side-bar/add-task/addTask';
 
 
 function Main() {
@@ -10,15 +11,21 @@ function Main() {
         <div className="mainSection">
             <h1>Untitled</h1>
             <div className="taskHolder">
-                {tasks.map(task => (
-                    <Task
-                        key={task.id}
-                        id={task.id}
-                        title={task.title}
-                        priority={task.priority}
-                        completion={task.completion}
-                    />
-                ))}
+                {   
+                tasks.length > 0 ? (
+                    tasks.map(task => (
+                        <Task
+                            key={task.id}
+                            id={task.id}
+                            title={task.title}
+                            priority={task.priority}
+                            completion={task.completion}
+                        />
+                    ))
+                ) : (
+                    <p style={ {marginLeft: 40, fontSize: 18} }>Let's start by pressing <strong>Add task</strong></p>
+                )
+             }
             </div>
         </div>
     );
